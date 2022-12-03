c5e19f475d0d874267ae63e93895051e68e8ce2a
Alexarzhanov288 <a9137771514@gmail.com>
2022-12-03 19:01:31 +0700

    Решил задачу 2

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.Clear();
+﻿// Задача 2
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < b)
+    Console.WriteLine($"max = {b}, min = {a}");
+else if (a == b)
+    Console.WriteLine("Числа равны");
+else
+    Console.WriteLine($"max = {a}, min = {b}");
---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Задача 4
+        Console.Clear();
+        Console.Write("Введите первое число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите третье число: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        if (a > b && a > c)
+            Console.WriteLine($"max = {a}");
+        else if (a < b && c < b)
+            Console.WriteLine($"max = {b}");
+        else if (a < c && b < c)
+            Console.WriteLine($"max = {с}");
+    }
+}
